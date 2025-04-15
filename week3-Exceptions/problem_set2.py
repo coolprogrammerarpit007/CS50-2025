6aa42a6c262a0bce06e5f6bf7ec9e07adee1da51
arpit mishra <arpit.mishra.out@gmail.com>
2025-04-15 08:32:06 +0530

    Menu Price Problem

--- week3-Exceptions/problem_set2.py
+++ week3-Exceptions/problem_set2.py
@@ -1,24 +1,64 @@
+import sys
 
+# while True:
+#     fraction = input("Fractions: ")
+#     try:
+#         number1 , number2 = fraction.split("/")
+#         frac1 = int(number1)
+#         frac2 = int(number2)
+#         if frac1 > frac2:
+#             continue
+#         percentage = (frac1/frac2) * 100
+#
+#     except ValueError:
+#         pass
+#     except ZeroDivisionError:
+#         pass
+#
+#     else:
+#         if percentage >= 99:
+#             print("F")
+#         elif percentage <= 1:
+#             print("E")
+#         else:
+#             print(f"{int(percentage)}%")
+#         break
+
+
+
+#  Problem 2
+
+menus = {
+"Baja Taco": 4.25,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
+}
+
+total_price = 0
 while True:
-    fraction = input("Fractions: ")
+    new_str = ""
     try:
-        number1 , number2 = fraction.split("/")
-        frac1 = int(number1)
-        frac2 = int(number2)
-        if frac1 > frac2:
-            continue
-        percentage = (frac1/frac2) * 100
+        items = input("Item: ").strip()
+        list_words = [word[0].capitalize() + word[1:] for word in items.split(" ")]
+        new_str = " ".join(list_words)
+        total_price += menus[new_str]
 
-    except ValueError:
+
+    except KeyError:
         pass
-    except ZeroDivisionError:
-        pass
+    except EOFError:
+        sys.exit(0)
 
     else:
-        if percentage >= 99:
-            print("F")
-        elif percentage <= 1:
-            print("E")
-        else:
-            print(f"{int(percentage)}%")
-        break
+        print(f"Total: ${total_price:.2f}")
+
+
+
+
+
