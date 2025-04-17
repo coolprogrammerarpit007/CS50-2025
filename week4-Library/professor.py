8f5629c2c362a38d7fd1eec15c998e8efd849e83
arpit mishra <arpit.mishra.out@gmail.com>
2025-04-17 07:51:17 +0530

    Refactor the code, make it more structured  and clean and changes from while to for loop

--- week4-Library/professor.py
+++ week4-Library/professor.py
@@ -1,86 +1,68 @@
 import random
-import sys
 
 
 def main():
-    get_level()
+    level = int(get_level())
+    game_score = 0
+    for i in range(0,10):
+        num1 = generate_integer(level)
+        num2 = generate_integer(level)
+        result =  num1 + num2
+        # print(result)
+
+        for index in range(0,3):
+            answer = (input(f"{num1} + {num2} = "))
+            if answer.isdigit():
+                if result == int(answer):
+                    game_score += 1
+                    break
+                if index == 2:
+                    print(f"{num1} + {num2} = {result}")
+                if result != int(answer):
+                    print("EEE")
+                    continue
+            else:
+                print("EEE")
+                if index == 2:
+                    print(f"{num1} + {num2} = {result}")
+                continue
+
+    print(f"Score: {game_score}")
+
+
+
 
 
 def get_level():
-    while True:
-        try:
+    try:
+        while True:
             level = input("Level: ")
-            if level.isdigit() and level == '1' or level == '2' or level == '3':
-                return generate_integer(int(level))
+            if (level == "1" or level == "2" or level == "3") and int(level) > 0:
+                return level
 
-        except ValueError:
-            continue
+            else:
+                continue
+
+    except ValueError:
+        print("Invalid Value")
 
 
-def generate_random_integer(num1, num2):
-    rand_number = random.randint(num1, num2)
-    return rand_number
+
+
 
 
 def generate_integer(level):
-    start_point = None
-    end_point = None
-    user_score = 0
-    wrong_answer = False
-    wrong_attempts = 0
-    hold_values = []
-    index = 0
-    if level == 1:
-        start_point = 1
-        end_point = 9
-
-    elif level == 2:
-        start_point = 10
-        end_point = 99
-
-    else:
-        start_point = 100
-        end_point = 999
-
-    while index < 10:
-        if not wrong_answer:
-            wrong_attempts = 0
-            number1 = generate_random_integer(start_point,end_point)
-            number2 = generate_random_integer(start_point,end_point)
-            hold_values.append(number1)
-            hold_values.append(number2)
-
-        try:
-            if wrong_attempts != 3:
-                answer = int(input(f"{hold_values[0]} + {hold_values[1]} = "))
-
-                if answer == (hold_values[0] + hold_values[1]):
-                    user_score += 1
-                    index += 1
-                    hold_values.clear()
-                    continue
-                else:
-                    wrong_answer = True
-                    wrong_attempts += 1
-                    continue
-
-            else:
-                index += 1
-                print(f"{hold_values[0]} + {hold_values[1]} = {hold_values[0] + hold_values[1]} ")
-                hold_values.clear()
-                wrong_answer = False
-                continue
-
-
-
-
-        except ValueError:
-            wrong_attempts += 1
-            wrong_answer = True
-            continue
-
-    print(f"Score: {user_score} ")
-
+    try:
+        if level == 1:
+            return random.randint(0,9)
+        elif level == 2:
+            return random.randint(10,99)
+        elif level == 3:
+            return random.randint(100,999)
+        else:
+            raise ValueError
+    except ValueError:
+        return -1
 
 
 if __name__ == "__main__":
